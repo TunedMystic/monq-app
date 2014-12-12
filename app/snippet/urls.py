a536d2d8a23b18fc6e8bcd28369515c244a5f0d2
Sandeep J <jsanweb@gmail.com>
2014-12-12 05:47:34 +0000

    Snippet ListView with Pagination.

--- app/snippet/urls.py
+++ app/snippet/urls.py
@@ -3,6 +3,41 @@ from django.views.generic import TemplateView
 import views
 
 urlpatterns = patterns("",
+  # Template for new Snippet.
   url(r"^new/$", TemplateView.as_view(template_name = "snippet/new.html"), name = "new"),
-  url(r"^newSnippet/$", views.SnippetCreateView.as_view(), name = "newSnippet")
+  # Create a new Snippet (AJAX)
+  url(r"^newSnippet/$", views.SnippetCreateView.as_view(), name = "newSnippet"),
+  # View (and search) all Snippets.
+  #url(r"^snippets/$", views.SnippetListView.as_view(), name = "listSnippet"),
+  #url(r"^snippets(/y/(?P<year>\d{4}))?(/m/(?P<month>0?[1-9]|1[012]))?/$", views.SnippetListView.as_view(), name = "listSnippet"),
+  url(r"^snippets(/y/(?P<year>[\d]{4}))?(/m/(?P<month>0?[1-9]|1[012]))?(/pg/(?P<page>[\d]+))?/$", views.SnippetListView.as_view(), name = "listSnippet"),
+  # View a single Snippet.
+  url(r"^(?P<urlcode>[\w-]+)/$", views.SnippetDetailView.as_view(), name = "detailSnippet"),
 )
+
+"""
+/new/           template for a new snippet
+/newSnippet/    post a new snippet (AJAX)
+/snippets/...   view and search snippets
+/<urlcode>/     view a single snippet
+/<urlode>/raw/  view the raw text of a snippet
+/<urlcode/copy/ copy an existing (public) snippet
+/u/<usrname>/   view a user's profile
+/me/            view your dashboard
+----------------------------------------------------
+
+/snippets/y/2013/m/3/
+
+/snippets/(?P<month>/m/[])
+^m/(0?[1-9]|1[012])$   # Month.
+
+^(?P<month>m/(0?[1-9]|1[012]))$   # Month.
+^(?P<year>y/(\d{4}))$        # year
+
+^snippets(?P<year>/y/(\d{4}))?/(?P<month>m/(0?[1-9]|1[012]))?/$
+
+^snippets(/y/(?P<year>\d{4}))?(/m/(?P<month>0?[1-9]|1[012]))?/$
+
+
+"""
+
--- app/snippet/views.py
+++ app/snippet/views.py
@@ -1,7 +1,8 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, ListView
+from .models import Snippet
 from .forms import SnippetForm
 
 class SnippetCreateView(CreateView):
@@ -40,5 +41,35 @@ class SnippetCreateView(CreateView):
   def form_invalid(self, form):
     return HttpResponseBadRequest(json.dumps(form.errors), content_type = "application/json")
     #return super(SnippetCreateView, self).form_invalid(form)
+
+
+class SnippetDetailView(DetailView):
+  """
+  View a single Snippet.
+  """
+  template_name = "snippet/single.html"
   
+  def _allowed_methods(self):
+    return ["get"]
+  
+  def get_object(self):
+    """
+    Get Snippet based on url parameter 'urlcode'.
+    """
+    snippet = get_object_or_404(Snippet, url_code = self.kwargs.get("urlcode", None))
+    return snippet
+
+
+class SnippetListView(ListView):
+  model = Snippet
+  template_name = "snippet/all.html"
+  paginate_by = 4
+  page_kwarg = "page"
+  
+  def get_queryset(self):
+    """
+    Return the list of items for this view.
+    """
+    return self.model.objects.all()
+    return super(SnippetListView, self).get_queryset(self)
   
