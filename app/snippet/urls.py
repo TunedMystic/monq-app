26a8d002f6c549326a4eb1c656b82ca320b81de4
Sandeep J <jsanweb@gmail.com>
2014-12-13 09:48:44 +0000

    Snippet Views, Pagination, Request.session Searching.

--- app/snippet/urls.py
+++ app/snippet/urls.py
@@ -5,12 +5,14 @@ import views
 urlpatterns = patterns("",
   # Template for new Snippet.
   url(r"^new/$", TemplateView.as_view(template_name = "snippet/new.html"), name = "new"),
-  # Create a new Snippet (AJAX)
+  # Create a new Snippet (AJAX).
   url(r"^newSnippet/$", views.SnippetCreateView.as_view(), name = "newSnippet"),
-  # View (and search) all Snippets.
-  #url(r"^snippets/$", views.SnippetListView.as_view(), name = "listSnippet"),
-  #url(r"^snippets(/y/(?P<year>\d{4}))?(/m/(?P<month>0?[1-9]|1[012]))?/$", views.SnippetListView.as_view(), name = "listSnippet"),
-  url(r"^snippets(/y/(?P<year>[\d]{4}))?(/m/(?P<month>0?[1-9]|1[012]))?(/pg/(?P<page>[\d]+))?/$", views.SnippetListView.as_view(), name = "listSnippet"),
+  # View all Snippets (optional page token for pagination).
+  url(r"^snippets(?:/(?P<pg>\d+))?/$", views.SnippetListView.as_view(), name = "allSnippet"),
+  # Search Snippets.
+  ## When this template is being served, clear session search variables.
+  url(r"^search/$", views.SnippetSearchFormView.as_view(template_name = "snippet/searchForm.html"), name = "searchFormSnippet"),
+  url(r"^search/results(?:/(?P<pg>[\d]+))?/$", views.SnippetSearchView.as_view(), name = "searchSnippet"),
   # View a single Snippet.
   url(r"^(?P<urlcode>[\w-]+)/$", views.SnippetDetailView.as_view(), name = "detailSnippet"),
 )
--- app/snippet/views.py
+++ app/snippet/views.py
@@ -1,11 +1,25 @@
 import json
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.views.generic import CreateView, DetailView, ListView
-from .models import Snippet
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, Http404
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView, DetailView, ListView, TemplateView
+from django.views.generic.edit import FormMixin, ProcessFormView
+from django.db.models import F
+from .models import Snippet, SnippetExtras
 from .forms import SnippetForm
 
-class SnippetCreateView(CreateView):
+
+class qSession(object):
+  """
+  Simple mixin to clear the 'searchSnippetQ' session variable.
+  """
+  def dispatch(self, request, *args, **kwargs):
+    # Delete the search query in request.session.
+    request.session.pop("searchSnippetQ", None)
+    return super(qSession, self).dispatch(request, *args, **kwargs)
+
+
+class SnippetCreateView(qSession, CreateView):
   """
   Create a Snippet.
   """
@@ -36,11 +50,9 @@ class SnippetCreateView(CreateView):
       "next": savedSnippet.url_code
     }
     return HttpResponse(json.dumps(d), content_type = "application/json")
-    #return super(SnippetCreateView, self).form_valid(form)
   
   def form_invalid(self, form):
     return HttpResponseBadRequest(json.dumps(form.errors), content_type = "application/json")
-    #return super(SnippetCreateView, self).form_invalid(form)
 
 
 class SnippetDetailView(DetailView):
@@ -57,19 +69,72 @@ class SnippetDetailView(DetailView):
     Get Snippet based on url parameter 'urlcode'.
     """
     snippet = get_object_or_404(Snippet, url_code = self.kwargs.get("urlcode", None))
+    # Update the 'hits' counter.
+    SnippetExtras.objects.filter(pk = snippet.snippetextras.id).update(hits = F("hits") + 1)
     return snippet
 
 
-class SnippetListView(ListView):
+class SnippetListView(qSession, ListView):
+  """
+  List all Snippets (with pagination).
+  """
   model = Snippet
   template_name = "snippet/all.html"
   paginate_by = 4
-  page_kwarg = "page"
+  page_kwarg = "pg"
   
   def get_queryset(self):
     """
     Return the list of items for this view.
     """
-    return self.model.objects.all()
-    return super(SnippetListView, self).get_queryset(self)
+    return self.model.objects.all().order_by("-_date_added")
+
+
+class SnippetSearchFormView(qSession, TemplateView):
+  def get(self, request, *args, **kwargs):
+    return super(SnippetSearchFormView, self).get(request, *args, **kwargs)
+
+
+class SnippetSearchView(ListView, ProcessFormView):
+  """
+  Search Snippets (with pagination).
+  """
+  model = Snippet
+  template_name = "snippet/search.html"
+  paginate_by = 3
+  page_kwarg = "pg"
   
+  def dispatch(self, request, *args, **kwargs):
+    """
+    Set request.method to 'POST' if session is found.
+    """
+    if not request.method == "POST":
+      request.method = "POST"
+    return super(SnippetSearchView, self).dispatch(request, *args, **kwargs)
+  
+  def post(self, request, *args, **kwargs):
+    """
+    Process search results.
+    """
+    # Overwrite the session variable.
+    if request.POST.get("searchQ", None):
+      request.session["searchSnippetQ"] = request.POST["searchQ"]
+    
+    self.object_list = self.get_queryset()
+    # No search results. Redirect to Search form.
+    if isinstance(self.object_list, type(None)):
+      return HttpResponseRedirect(reverse("snippet:searchFormSnippet"))
+    return self.render_to_response(self.get_context_data())
+  
+  def get_queryset(self):
+    """
+    Return the list of items for this view.
+    """
+    searchQ = self.request.POST.get("searchQ", None) or self.request.session.get("searchSnippetQ", None)
+    # Niether the POST parameter or the Session variable was found.
+    # In which case, return None.
+    if not searchQ:
+      return None
+    
+    results = self.model.objects.filter(title__icontains=searchQ).distinct()
+    return results
