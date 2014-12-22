bf4324b5b0b63fa2b82c4fc0c79ce8d24719bdb2
Sandeep <jadoonanansandeep@gmail.com>
2014-12-22 05:30:34 -0500

    Work on About page.

--- app/nil/urls.py
+++ app/nil/urls.py
@@ -4,6 +4,7 @@ import views
 
 urlpatterns = patterns("",
   #url(r"^.*$", TemplateView.as_view(template_name = "nil/index.html"), name = "index"),
+  url(r"^about/$", TemplateView.as_view(template_name = "nil/about.html"), name = "about"),
   url(r"^404/$", views.error404, name = "error404"),
   url(r"^500/$", views.error500, name = "error500"),
 )
