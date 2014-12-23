7d2f58fe0585ce62bdb333da14cc1d37f07e7a2b
Sandeep J <jsanweb@gmail.com>
2014-12-23 09:03:33 +0000

    Move entire project up one directory.

--- webapp/settings.py
+++ webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '^ujom94*or-!h7!l2e=f)+_$)*0=!wzq@_kb)u_ar90u)v-szu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
