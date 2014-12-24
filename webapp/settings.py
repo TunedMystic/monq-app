51913b8d1aaae866b07bee2455a57e5018ccab71
Sandeep J <jsanweb@gmail.com>
2014-12-24 14:32:54 +0000

    Managing fonts.

--- webapp/settings.py
+++ webapp/settings.py
@@ -27,7 +27,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 if not DEBUG:
   #ALLOWED_HOSTS = [".nitrousbox.com", "127.0.0.1", "0.0.0.0", "localhost"]
-  ALLOWED_HOSTS = ["*"]
+  ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
