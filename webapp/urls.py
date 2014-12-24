b49d37e875e55703dfbaf18c3e6c5b1f9ad48d58
Sandeep J <jsanweb@gmail.com>
2014-12-24 14:20:32 +0000

    misc changes.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
   
@@ -13,7 +14,7 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
 
 handler404 = "nil.views.error404"
